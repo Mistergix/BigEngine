08443fae5241fea457de998a827769ddf8cd845b
Nikow <nicolas@ruche.me>
2022-05-10 18:54:25 +0200

    feat(mainloop); begin main loop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include "core/game_time.h"
 #include "core/object.h"
-#include "core/game_object.h"
-#include "core/components/transform.h"
-#include "scenes/scene.h"
 #include "editor/editor.h"
+#include "core/game.h"
 
-
-void HandleInput();
 
 int main() {
     // TODO main loop
@@ -16,39 +10,9 @@ int main() {
     // TODO gérer les id dans les fichiers
 
     Editor::Init();
+    Game game;
 
-
-    std::string scenePath = "Assets/Scenes/SceneTest.scene";
-    Scene scene(scenePath);
-    std::cout << scene.name() << std::endl;
-
-    auto gos = FIND_OBJECTS_OF_TYPE(Transform);
-    auto gosWithTag = GameObject::FindObjectsWithTag("Bibi");
-
-    std::cout << gos->size() << "of type Transform" <<  std::endl;
-
-    std::cout << gosWithTag->size() << "with tag bibi" <<  std::endl;
-
-    
-
-    /*
-    Time time;
-    auto isRunning = true;
-    while(isRunning){
-        HandleInput();
-        LogicUpdate();
-        PhysicsUpdate();
-        Render();
-
-        time.UpdateTime();
-        if(time.CurrentTime() > 10.0){
-            isRunning = false;
-        }
-    }*/
+    game.Run();
     return 0;
 }
 
-void HandleInput() {
-    // Utiliser une lib ?
-    // Stocker et dater les inputs
-}
