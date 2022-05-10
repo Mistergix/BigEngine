7f171f9c8c4543d1f3f24b3f3b1f896ddeb95f5b
etienne-gibiat <54933278+etienne-gibiat@users.noreply.github.com>
2022-05-10 18:33:13 +0200

    opengl

--- main.cpp
+++ main.cpp
@@ -6,11 +6,50 @@
 #include "scenes/scene.h"
 #include "editor/editor.h"
 
+#include "Glew/glew.h"
+#include "Glfw/glfw3.h"
+
 
 void HandleInput();
 
+void ErrorCallback(int error, const char* desc){
+    std::cerr << "Error" << desc << std::endl;
+}
+
 int main() {
     Editor::Init();
+
+    if(!glfwInit()){
+        std::cerr << "gltf init fail" << std::endl;
+        return -1;
+    }
+
+    glfwSetErrorCallback(ErrorCallback);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    GLFWwindow* p_window = glfwCreateWindow(1280, 720, "test", nullptr, nullptr);
+
+    glfwMakeContextCurrent(p_window);
+
+    glfwSwapInterval(1);
+    glEnable(GL_CULL_FACE);
+
+    glewExperimental = GL_TRUE;
+
+    if(glewInit() != GLEW_OK){
+        glfwTerminate();
+        std::cerr << "Glew init Fail" << std::endl;
+        return  -1;
+    }
+
+    while (!glfwWindowShouldClose(p_window) && glfwGetKey(p_window, GLFW_KEY_ESCAPE) != GLFW_PRESS){
+
+        glfwSwapBuffers(p_window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 
 
     std::string scenePath = "Assets/Scenes/SceneTest.scene";
